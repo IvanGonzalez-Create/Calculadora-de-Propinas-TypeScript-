a5d962e2f696d0f592d51d15b9d4668470547a04
Ivan <ivangonzalezsl123@gmail.com>
2025-10-07 23:48:43 -0300

    Añadido de estilos al menu de items

--- src/App.tsx
+++ src/App.tsx
@@ -7,38 +7,40 @@ function App() {
   return (
 
     <>
+
     <header className=" bg-teal-400 py-5 ">
-      <h1 
-      className="text-4xl md:text-5xl font-extrabold tracking-tight text-gray-900 dark:text-white text-center leading-tight"
+      <h1 className="text-4xl md:text-5xl font-extrabold tracking-tight text-gray-900 dark:text-white text-center leading-tight"
       >Calculadora de Propinas y Consumo
       </h1>
     </header>
   
-    <main className="max-w-7xl mx-auto py-20 grid md:grid-cols-2">
-  <div>
+    <main className="max-w-7xl mx-auto py-20 grid md:grid-cols-2 gap-12 px-4 ">
+
+      <div>
      
-      <h2>Menu</h2>
-
-      {menuItems.map(item => (
-        <MenuItems 
-        />
-  
-      ))}
-
+      <h2 className="text-lg font-semibold text-sky-700 tracking-wide uppercase border-b-2 border-sky-300 pb-1">Menu</h2>
+        <div className="space-y-4">
+          {menuItems.map(item => (
+            <MenuItems 
+            key={item.id}
+            item = {item}
+            />
+          ))}   
+       </div>  
       
-      
-  </div>
+      </div>
 
 
   
-  <div>
+    <div>
     
-    <h2>Consumo</h2> 
+      <h2>Consumo</h2> 
 
-  </div>
+    </div>
 
-    </main>
-    </>
+  </main>
+    
+  </>
 
   )
 }
--- src/components/MenuItems.tsx
+++ src/components/MenuItems.tsx
@@ -1,11 +1,23 @@
-export default function MenuItems() {
+import type { Foods } from "../types"
+
+type MenuitemsProps = {
+    item : Foods
+
+}
+
+
+export default function MenuItems({item} : MenuitemsProps) {
 
 
 
 
   return (
 
-    <div>MenuItems</div>
+<button className="w-full flex justify-between items-center bg-white rounded-xl shadow-lg px-5 py-4 hover:shadow-2xl hover:bg-sky-50 transition-all duration-300">
+  <p className="text-sky-800 font-medium">{item.name}</p>
+  <p className="text-sky-700 font-semibold">${item.price}</p>
+</button>
+
 
   )
 }
